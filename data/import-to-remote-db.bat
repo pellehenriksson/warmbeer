dab8da56ff38124184abccc64f2407bec2ed52e7
Per Henriksson <perh@bluegarden.com>
2016-09-16 13:30:31 +0200

    cleanup

--- data/import-to-remote-db.bat
+++
@@ -1,8 +0,0 @@
-
-bcp [warmbeer-db].dbo.Stores in stores.dat -E -C RAW -T -c -U pelle@warmbeer-sqlserver -S tcp:warmbeer-sqlserver.database.windows.net,1433 -P Rokkahart666
-
-bcp [warmbeer-db].dbo.Items in items.dat -E -C RAW -T -c -U pelle@warmbeer-sqlserver -S tcp:warmbeer-sqlserver.database.windows.net,1433 -P Rokkahart666
-
-bcp [warmbeer-db].dbo.StoreItems in storeitems.dat -E -C RAW -T -c -U pelle@warmbeer-sqlserver -S tcp:warmbeer-sqlserver.database.windows.net,1433 -P Rokkahart666
-
-pause
