8011a065c1368cbe3a67c88d0b5726f138e1350f
Per Henriksson <perh@bluegarden.com>
2016-09-16 10:38:05 +0200

    data import

--- data/import-to-remote-db.bat
+++ data/import-to-remote-db.bat
@@ -1,6 +1,6 @@
-sqlcmd -S tcp:warmbeer-sqlserver.database.windows.net -d [warmbeer-db] -U pelle@warmbeer-sqlserver -P Rokkahart666  /Q "delete from storeitems;delete from items;delete from stores;"
+# sqlcmd -S tcp:warmbeer-sqlserver.database.windows.net -d [warmbeer-db] -U pelle@warmbeer-sqlserver -P Rokkahart666  /Q "delete from storeitems;delete from items;delete from stores;"
 
-bcp [warmbeer-db.dbo.Stores in stores.dat -C RAW -E -T -c -U pelle@warmbeer-sqlserver -S tcp:warmbeer-sqlserver.database.windows.net -P Rokkahart666
+bcp [warmbeer-db].dbo.Stores in stores.dat -C RAW -E -T -c -U pelle@warmbeer-sqlserver -S tcp:warmbeer-sqlserver.database.windows.net -P Rokkahart666
 bcp [warmbeer-db].dbo.Items in items.dat -E -C RAW -T -c -U pelle@warmbeer-sqlserver -S tcp:warmbeer-sqlserver.database.windows.net -P Rokkahart666
 bcp [warmbeer-db].dbo.StoreItems in storeitems.dat -E -C RAW -T -c -U pelle@warmbeer-sqlserver -S tcp:warmbeer-sqlserver.database.windows.net -P Rokkahart666
 
